5f221910a0f4df7450c090dcf1ae8e0c929ffb16
Chokundev <94124435+Chokundev@users.noreply.github.com>
2023-10-17 22:12:27 +0700

    Update UI BN-Esport & Whiteclass Projects Webpage

---
+++ projects/whiteclass/js/script.js
@@ -0,0 +1,13 @@
+console.log("Connect JS Success 🟢");
+
+function btn1(){
+    document.getElementById("img").src = "./images/1.svg" ;
+}
+
+function btn2(){
+    document.getElementById("img").src = "./images/2.svg" ;
+}
+
+function btn3(){
+    document.getElementById("img").src = "./images/3.svg" ;
+}
