f5248d63dd7a582b7413e8425ba67830ad145eb0
Chokundev <94124435+Chokundev@users.noreply.github.com>
2023-10-17 22:28:46 +0700

    Update Advertise Webpage UI

---
+++ projects/advertise/js/script.js
@@ -0,0 +1,21 @@
+console.log("Connect JS Success 🟢");
+
+function btn1(){
+    document.getElementById("img").src = "./images/4.svg" ;
+}
+
+function btn2(){
+    document.getElementById("img").src = "./images/5.svg" ;
+}
+
+function btn3(){
+    document.getElementById("img").src = "./images/8.svg" ;
+}
+
+function btn4(){
+    document.getElementById("img").src = "./images/6.svg" ;
+}
+
+function btn5(){
+    document.getElementById("img").src = "./images/7.svg" ;
+}
